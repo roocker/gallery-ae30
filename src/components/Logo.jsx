3f4224cf94251b0fe77e9a2e058209a7817093ff
roocker <flosspam@gmail.com>
2023-06-06 14:33:31 +0200

    broken node_modules

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,13 +4,15 @@ import {motion} from 'framer-motion';
 function Logo (){
   return (
   <motion.h1>
-    <a class="link" menu-toggle>
+    <a className="link">
       <span>AE30 Architekten - Kratochwil Gerhard, Waldbauer Peter, Zeinitzer Klaus</span>
-      <div class="logo">
-        <img src="/ae30-logo.svg" alt="AE30 Architekten - Kratochwil Gerhard, Waldbauer Peter, Zeinitzer Klaus" />
-      </div>
+    <svg className="logo">
+      <use id="use_logo" className="svg-logo-use" href="/logo.svg#logo_symbol" title="asdf" />
+    </svg>
     </a>
   </motion.h1> 
   )}
+  // <img src="/logo.svg#logo_symbol" alt=""/>
+// <img src="/ae30-logo.svg" alt="AE30 Architekten - Kratochwil Gerhard, Waldbauer Peter, Zeinitzer Klaus" />
 
 export default Logo
