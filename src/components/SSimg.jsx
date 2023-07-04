990e15c531d6916c572f5c2aa271db99c4ba9539
roocker <flosspam@gmail.com>
2023-07-04 11:43:37 +0200

    reworked slideshow for zoom animation + zoom drag, stoggle animation not really satisfying

---
+++ src/components/SSimg.jsx
@@ -0,0 +1,100 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import "../styles/slideshow.css";
+
+const animation_zoom = {
+  i:{
+    minHeight: "0vh",
+    minWidth: "0vw",
+  },
+  a:{
+    minHeight: "100vh", 
+    minWidth: "100vw", 
+  },
+  e:{
+    minHeight: "0vh",
+    minWidth: "0vw",
+  }
+}
+
+function SSimg ( props ) {
+
+  const images = props.images;
+  const index = props.index;
+  const alts = props.alts;
+  //Toggle for Zoom
+  const zToggle = props.zoom;
+
+
+  /* console.log("current images:", images)
+  console.log("current index:", images[index])
+  console.log("current alt:", alts[index])
+  console.log("current zoom state:", zToggle) */
+
+  // Zoom Drag feature
+  /* #rev #bug
+  - need to save downX as useState to have zoom feature whenever image is switched to next one 
+  - also need to update img (autoplay)
+  - vl spart man sich dadurch auch das ganze nextPercentage gedöns?
+  - case TouchEvent und so
+
+  */
+
+  const img = document.getElementById('img');
+  
+  console.log(img)
+  const zoomDragUp = () =>{
+    console.log("up", img.dataset.downX)
+    img.dataset.downX = 0;
+    img.dataset.prevPercentage = img.dataset.downX;
+  }
+  const zoomDragDown = (e) => {
+      props.onClick();
+      if (e.nativeEvent instanceof MouseEvent) {
+        img.dataset.downX = e.clientX;
+        console.log("zoomDragDown x:",img.dataset.downX)
+      }
+    }
+  const zoomDragMove = (e) =>{
+    if (zToggle && e.nativeEvent instanceof MouseEvent) {
+      const moveXdelta = img.dataset.downX - e.clientX;
+      const maxXdelta = window.innerWidth;
+      const moveXdeltaPercent = ( moveXdelta / maxXdelta  ) * -100  ;
+      const nextPercentage = img.dataset.prevPercentage + moveXdeltaPercent 
+      return (
+        img.animate(
+          {objectPosition: `${nextPercentage}% 50%`},
+          {duration: 1200, fill: "forwards"},
+        )
+      ); 
+    }
+  }
+
+  return(
+    // <div class="img">
+    <AnimatePresence initial={true}>
+    <motion.img
+    className=""
+    id="img"
+    src={images[index]}
+    alt={alts[index]}
+    variants={animation_zoom}
+    exit="e"
+    initial={zToggle ? "a" : "i"}
+    animate={zToggle ? "a" : "i"}
+    transition={{ type: 'spring', stiffness: 200, damping: 20 }}
+
+    // onClick={zoomDragDown}
+    onMouseDown={zoomDragDown}
+    onMouseMove={zoomDragMove}
+    onMouseUp={zoomDragUp}
+
+    draggable={false}
+
+    />
+    </AnimatePresence>
+    // </div>
+
+  )
+
+}
+export default SSimg;
