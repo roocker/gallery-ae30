d77a308121d8fd98b58f7f415c7e8f2c9f8faa2e
Florian Schwegel <flo@schwegel.at>
2023-10-24 09:56:02 +0200

    trying to fix filter bugs

--- src/components/GridProj.jsx
+++ src/components/GridProj.jsx
@@ -1,5 +1,6 @@
 /* 
   TODO
+  HEUTE
   - [ ] figcaption visual bug when text wrap
   - [ ] Picture crop/resize position from CMS / Collection as optional attrib!
   https://docs.astro.build/en/guides/integrations-guide/image/#picture-
@@ -7,72 +8,69 @@
   - [ ] sharp image operation (color correction?)
 
   */
-import { useStore } from '@nanostores/react';
-import { AnimatePresence, motion} from "framer-motion";
-import { handleRemoveFilter, stateCurrentProjs } from '../states';
+import { useStore } from "@nanostores/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { handleRemoveFilter, stateCurrentProjs } from "../states";
 
-import '../styles/grid.css';
-import React, { useEffect } from 'react';
-
+import "../styles/grid.css";
+import React, { useEffect } from "react";
 
 // Pictures Aspect Ratio
 const width = 3;
 const height = 2;
 
-function GridProj (props) {
-
+function GridProj(props) {
   let projs = [];
   const allProjs = props.projects;
 
   const currentProjs = useStore(stateCurrentProjs);
-  console.log("currentProjs", currentProjs)
+  console.log("currentProjs", currentProjs);
 
   const imgSrcs = props.covers;
   // console.log("imgSrcs", imgSrcs)
 
+  if (currentProjs) {
+    projs = currentProjs;
+    console.log("SUCESS: GridProj found Projects in States", projs);
+  } else {
+    projs = allProjs;
+    console.log(
+      "WARNING: GridProj didn't fount Projects from States, default to allProjects",
+    );
+    stateCurrentProjs.set(projs);
+  }
 
-
-    if(currentProjs){
-      projs = currentProjs
-      console.log("SUCESS: GridProj found Projects in States" , projs);
-    } else {
-      projs = allProjs
-      console.log("WARNING: GridProj didn't fount Projects from States, default to allProjects");
-      stateCurrentProjs.set(projs);
-    }
-
-
-  console.log("projs" , projs)
-
-
-
+  console.log("projs", projs);
 
   let completedAnimations = 0;
   const totalAnimations = projs.length;
-
   // console.log("gibts noch projs?", projs)
 
   // #bug #rev a href trigger reset!!!
-  if(projs == 0){
+  if (projs == 0) {
     return (
-      <div 
-      className="noProjects"
-      >
-      <p>Es sind keine Projekte für die gewählten Filter Einstellungen vorhanden.</p>
-      <p><a href="#" onClick={handleRemoveFilter}>Filter entfernen</a> oder <a href="">Anpassen</a></p>
-      
+      <div className="noProjects">
+        <p>
+          Es sind keine Projekte für die gewählten Filter Einstellungen
+          vorhanden.
+        </p>
+        <p>
+          <a href="#" onClick={handleRemoveFilter}>
+            Filter entfernen
+          </a>{" "}
+          oder <a href="">Anpassen</a>
+        </p>
       </div>
-    )
-  }else{
+    );
+  } else {
     return projs.map((proj, index) => {
-
       // console.log(proj, index);
 
       let smaller;
       const vh = window.innerHeight;
       const vw = window.innerWidth;
 
-      vw > vh ? smaller = vh : smaller = vw;
+      vw > vh ? (smaller = vh) : (smaller = vw);
 
       const min = smaller * -1;
       const max = smaller;
@@ -80,13 +78,13 @@ function GridProj (props) {
       const minR = -270;
       const maxR = 270;
 
-      const randomX = Math.random() * (max-min) + min;
+      const randomX = Math.random() * (max - min) + min;
       // console.log(randomX);
-      const randomY = Math.random() * (max-min) + min;
+      const randomY = Math.random() * (max - min) + min;
       // console.log(randomY);
-      const randomR = Math.random() * (maxR-minR) + minR;
+      const randomR = Math.random() * (maxR - minR) + minR;
       // console.log(randomR);
-      const randomS = Math.random() ; 
+      const randomS = Math.random();
       // console.log(randomS);
 
       const shuffle = {
@@ -94,7 +92,7 @@ function GridProj (props) {
           x: randomX,
           y: randomY,
           rotate: randomR,
-          scale: .8,
+          scale: 0.8,
         },
         a: {
           x: 0,
@@ -102,80 +100,76 @@ function GridProj (props) {
           scale: 1,
           rotate: 0,
           transition: {
-            type: 'spring', stiffness: 100, damping: 20,
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
             delay: index * 0.3 * randomS,
-          }
+          },
         },
         e: {
           x: randomX,
           y: randomY,
         },
-        z1:{
+        z1: {
           scale: 1.05,
           transition: {
-            duration: .1,
+            duration: 0.1,
             // type: 'spring', stiffness: 100, damping: 20,
           },
         },
-        z2:{
+        z2: {
           scale: 7,
           originY: "50%",
           originX: "50%",
           transition: {
-            type: 'spring', stiffness: 100, damping: 20,
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
           },
         },
-      }; 
-
+      };
 
       // console.log("weitheig:", proj.data.widehigh)
       const isWide = proj.data.widehigh === "extra breit" ? true : false;
       const isHigh = proj.data.widehigh === "extra hoch" ? true : false;
       // console.log("hier:", isWide, isHigh)
 
-
-
       return (
         <motion.article
-        className={`${isWide ? "wide" : ""} ${isHigh ? "high" : ""}`}
-        variants={shuffle}
-        key={index}
-        initial="i"
-        animate="a"
-        exit="e"
-        whileHover="z1"
-        // onHoverEnd={e => {}}
-        // whileTap="z2"
-        onAnimationComplete={() => {
-          completedAnimations++;
-          if (completedAnimations === totalAnimations) {
-            window.scrollTo(0, 0);
-          }
-        }}
+          className={`${isWide ? "wide" : ""} ${isHigh ? "high" : ""}`}
+          variants={shuffle}
+          key={index}
+          initial="i"
+          animate="a"
+          exit="e"
+          whileHover="z1"
+          // onHoverEnd={e => {}}
+          // whileTap="z2"
+          onAnimationComplete={() => {
+            completedAnimations++;
+            if (completedAnimations === totalAnimations) {
+              window.scrollTo(0, 0);
+            }
+          }}
         >
+          <a href={`../${proj.data.category}/${proj.slug}`}>
+            <AnimatePresence initial={true}>
+              <figure>
+                <img
+                  src={imgSrcs.find((img) => img.imgIndex === index).img.src}
+                  alt={proj.data.titleimg.alt}
+                />
 
-        <a href={`../${proj.data.category}/${proj.slug}`}>
-        <AnimatePresence
-        initial={true}
-        >
-
-        <figure>
-        <img 
-        src={imgSrcs.find((img) => img.imgIndex === index).img.src}
-        alt={proj.data.titleimg.alt}
-        />
-
-        <figcaption>{index} {proj.data.title}</figcaption>
-        </figure> 
-        </AnimatePresence>
-        </a> 
+                <figcaption>
+                  {index} {proj.data.title}
+                </figcaption>
+              </figure>
+            </AnimatePresence>
+          </a>
         </motion.article>
-      )
-    })
+      );
+    });
   }
 }
 
-
 export default GridProj;
-
-
