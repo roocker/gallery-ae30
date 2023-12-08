08ce3b19d7e0d3cbdd7f993f46b3730ed92f655c
roocker <flosspam@gmail.com>
2023-12-08 08:29:17 +0100

    changed textmodal, edited netlifycms settings

--- src/content/config.js
+++ src/content/config.js
@@ -1,48 +1,54 @@
 import { defineCollection, z } from "astro:content";
 
 const projCollection = defineCollection({
-
   type: "content",
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    title_l: z.string(),
-    title_zzl: z.string().optional(),
-    archived: z.string().optional(),
-    widehigh: z.string().optional(),
-    publishDate: z.string().transform((string) => new Date(string)),
-    category: z.string(),
-    project_keys: z.object({
-      status: z.string().optional(),
-      year: z.string().transform((string) => new Date(string)),
-      year2: z.string().transform((string) => new Date(string)),
-      area: z.string(),
-      tags: z.array(z.string()),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      title_l: z.string(),
+      title_zzl: z.string().optional(),
+      archived: z.string().optional(),
+      widehigh: z.string().optional(),
+      publishDate: z.string().transform((string) => new Date(string)),
+      category: z.string(),
+      project_keys: z.object({
+        status: z.string().optional(),
+        year: z.string().transform((string) => new Date(string)),
+        year2: z.string().transform((string) => new Date(string)),
+        area: z.string(),
+        tags: z.array(z.string()),
+      }),
+      titleimg: z.object({
+        img: image().refine((img) => img.width >= 400, {
+          message: "Cover image must be at least 1080 pixels wide!",
+        }),
+        alt: z.string(),
+      }),
+
+      fotos: z
+        .array(
+          z.object({
+            foto: image().refine((img) => img.width >= 400, {
+              message: "Cover image must be at least 1080 pixels wide!",
+            }),
+            alt: z.string(),
+          }),
+        )
+        .optional(),
+
+      plans: z
+        .array(
+          z.object({
+            plan: image().refine((img) => img.width >= 400, {
+              message: "Cover image must be at least 1080 pixels wide!",
+            }),
+            alt: z.string(),
+          }),
+        )
+        .optional(),
     }),
-    titleimg: z.object({
-      img: image().refine((img)  => img.width >= 400, {
-        message: "Cover image must be at least 1080 pixels wide!"
-      }),
-      alt: z.string(),
-    }),
-
-    fotos: z.array(z.object({
-      foto: image().refine((img)  => img.width >= 400, {
-        message: "Cover image must be at least 1080 pixels wide!"
-      }),
-      alt: z.string(),
-    })),
-
-    plans: z.array(z.object({
-      plan: image().refine((img)  => img.width >= 400, {
-        message: "Cover image must be at least 1080 pixels wide!"
-      }),
-      alt: z.string(),
-    })).optional(),
-  })
-})
-
+});
 
 export const collections = {
   projects: projCollection,
 };
-
