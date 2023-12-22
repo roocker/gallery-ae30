1fdde7d422afed54384095d2b5dd0a771880579b
roocker <flosspam@gmail.com>
2023-12-22 12:33:50 +0100

    edit: team, fixed image optimization and content type; wip: pages

--- src/content/config.js
+++ src/content/config.js
@@ -1,4 +1,50 @@
+import { string } from "astro/zod";
 import { defineCollection, z } from "astro:content";
+
+const teamCollection = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      teamfoto: z.object({
+        img: image().refine(img => img.width >= 700, {
+          message: "Teamfoto image must be at least 700 pixels wide!",
+        }),
+        alt: z.string(),
+      }),
+      text: z.string(),
+      heading_members_highlighted: z.string(),
+      heading_members_active: z.string(),
+      heading_members_former: z.string(),
+      member: z.array(
+        z.object({
+          name: z.string(),
+          img: image()
+            .refine(img => img.width >= 100, {
+              message: "Member image must be at least 300 pixels wide!",
+            })
+            .optional(),
+          mode: z
+            .object({
+              highlighted: z.boolean().optional(),
+              former: z.boolean().optional(),
+              archived: z.boolean().optional(),
+            })
+            .optional(),
+          details: z
+            .array(
+              z.object({
+                description: z
+                  .union([z.string(), z.number()])
+                  .nullable()
+                  .optional(),
+                detail: z.union([z.string(), z.number()]).nullable().optional(),
+              })
+            )
+            .optional(),
+        })
+      ),
+    }),
+});
 
 const projCollection = defineCollection({
   type: "content",
@@ -9,17 +55,17 @@ const projCollection = defineCollection({
       title_zzl: z.string().optional(),
       archived: z.boolean().optional(),
       widehigh: z.string().optional(),
-      publishDate: z.string().transform((string) => new Date(string)),
+      publishDate: z.string().transform(string => new Date(string)),
       category: z.string(),
       project_keys: z.object({
         status: z.string().optional(),
-        year: z.string().transform((string) => new Date(string)),
-        year2: z.string().transform((string) => new Date(string)),
+        year: z.string().transform(string => new Date(string)),
+        year2: z.string().transform(string => new Date(string)),
         area: z.string(),
         tags: z.array(z.string()),
       }),
       titleimg: z.object({
-        img: image().refine((img) => img.width >= 400, {
+        img: image().refine(img => img.width >= 400, {
           message: "Cover image must be at least 1080 pixels wide!",
         }),
         alt: z.string(),
@@ -28,22 +74,22 @@ const projCollection = defineCollection({
       fotos: z
         .array(
           z.object({
-            foto: image().refine((img) => img.width >= 400, {
+            foto: image().refine(img => img.width >= 400, {
               message: "Cover image must be at least 1080 pixels wide!",
             }),
             alt: z.string(),
-          }),
+          })
         )
         .optional(),
 
       plans: z
         .array(
           z.object({
-            plan: image().refine((img) => img.width >= 400, {
+            plan: image().refine(img => img.width >= 400, {
               message: "Cover image must be at least 1080 pixels wide!",
             }),
             alt: z.string(),
-          }),
+          })
         )
         .optional(),
     }),
@@ -51,4 +97,5 @@ const projCollection = defineCollection({
 
 export const collections = {
   projects: projCollection,
+  team: teamCollection,
 };
