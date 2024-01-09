9d9a8ff57110655eb2ddc07fbce86ed069cde6df
roocker <flosspam@gmail.com>
2024-01-09 22:10:53 +0100

    edit: SimilarProjects for Summary WIP

--- src/components/ProjectSummary.jsx
+++ src/components/ProjectSummary.jsx
@@ -1,26 +1,41 @@
-import { stateSlideshowIndex } from "../states.jsx";
+import { stateSlideshowIndex, stateSlideshow, stateModal } from "../states.jsx";
+import SimilarProjects from "./SimilarProjects.jsx";
+
 export default function ProjectSummary(props) {
-  console.log("summary,picture", props.pictureTitles);
-  console.log("pläne:", props.planTitles);
+  // console.log("summary,picture", props.pictureTitles);
+  // console.log("pläne:", props.planTitles);
 
   const children = props.children;
   const pictureTitles = props.pictureTitles;
   const planTitles = props.planTitles;
 
-  const setIndex = i => {
-    console.log("MOOOVE", i);
+  const setIndexFotos = i => {
+    stateSlideshow.set(true);
     stateSlideshowIndex.set(i);
+  };
+  const setIndexPlans = i => {
+    stateSlideshow.set(false);
+    stateSlideshowIndex.set(i);
+  };
+  const toggleModal = () => {
+    stateModal.set(true);
   };
 
   return (
     <div className="project_summary">
       {children}
 
+      <p className="read_more">
+        <a href="#" onClick={() => toggleModal()}>
+          mehr lesen...
+        </a>
+      </p>
+
       <h3> Fotos </h3>
       <ol>
         {pictureTitles.map((img, i) => (
           <li key={img}>
-            <a href="#" onClick={() => setIndex(i)}>
+            <a href="#" onClick={() => setIndexFotos(i)}>
               {img}
             </a>
           </li>
@@ -33,7 +48,7 @@ export default function ProjectSummary(props) {
           <ol>
             {planTitles.map((img, i) => (
               <li key={img}>
-                <a href="#" onClick={() => setIndex(i)}>
+                <a href="#" onClick={() => setIndexPlans(i)}>
                   {img}
                 </a>
               </li>
@@ -43,7 +58,7 @@ export default function ProjectSummary(props) {
       )}
 
       <h2>Ähnliche Projekte:</h2>
-      <div className="summary_nav"></div>
+      <SimilarProjects items="4" currentProj={props.currentProj} />
       <h2>
         <a href="/">Kehre zur Galerie zurück</a>
       </h2>
