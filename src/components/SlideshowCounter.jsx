d09cc4b66a0d6898982cfd02f9cbb5868fd50d05
roocker <flosspam@gmail.com>
2024-01-16 19:07:19 +0100

    edit: impl animation counter

--- src/components/SlideshowCounter.jsx
+++ src/components/SlideshowCounter.jsx
@@ -1,16 +1,69 @@
-import React from 'react';
-import { useStore } from '@nanostores/react';
-import { slideshow_length, stateSlideshowIndex } from '../states.jsx';
-import '../styles/counter.css'
+import React, { useEffect } from "react";
+import { useStore } from "@nanostores/react";
+import { slideshow_length, stateSlideshowIndex } from "../states.jsx";
+import {
+  AnimatePresence,
+  motion,
+  useSpring,
+  useTransform,
+} from "framer-motion";
+import "../styles/counter.css";
 
+const fontSize = 20;
+const padding = 18;
+const height = fontSize + padding;
 
-export default function SlideshowCounter (){
+export default function SlideshowCounter() {
   const index = useStore(stateSlideshowIndex);
   const length = useStore(slideshow_length);
-  return(
-    <p className="counter">
-    <span className="slideshow_number index">{index +1}</span>&mdash;<span className="slideshow_number length">{length}</span>
+  console.log("index", index, "/", length);
+
+  return (
+    <p
+      className={`counter ${length > 9 ? "twodigits" : ""}`}
+      style={{ fontSize }}
+    >
+      {length > 9 && <Digit place={10} val={index + 1} />}
+      <Digit place={1} val={index + 1} />
+      &mdash;
+      <div className="digit">
+        <span className="slideshow_number length">{length}</span>
+      </div>
     </p>
   );
 }
 
+function Digit({ place, val }) {
+  let valueRoundedToPlace = Math.floor(val / place);
+  let animatedValue = useSpring(valueRoundedToPlace);
+
+  useEffect(() => {
+    animatedValue.set(valueRoundedToPlace);
+  }, [animatedValue, valueRoundedToPlace]);
+
+  return (
+    <div style={{ height }} className="digit">
+      {[...Array(10).keys()].map(i => (
+        <Number mv={animatedValue} n={i} key={i} />
+      ))}
+    </div>
+  );
+}
+function Number({ mv, n }) {
+  let y = useTransform(mv, latest => {
+    let placeValue = latest % 10;
+    let offset = (10 + n - placeValue) % 10;
+    let memo = offset * height;
+
+    if (offset > 5) {
+      memo -= 10 * height;
+    }
+    return memo;
+  });
+
+  return (
+    <motion.span style={{ y }} className="slideshow_number index">
+      {n}
+    </motion.span>
+  );
+}
