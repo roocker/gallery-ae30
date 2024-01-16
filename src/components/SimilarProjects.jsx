afddc7fc073b3e031e29fa2be39038954aa59a87
roocker <flosspam@gmail.com>
2024-01-16 16:32:18 +0100

    edit: update ProjectSummary Page

--- src/components/SimilarProjects.jsx
+++ src/components/SimilarProjects.jsx
@@ -6,16 +6,16 @@ export default function SimilarProjects(props) {
   let items = props.items;
   // let currentProj = props.currentProj;
   const allProjects = props.allProjects;
-  console.log("allprojects ", allProjects);
+  // console.log("allprojects ", allProjects);
 
   let currentProj = allProjects.find(p => p.slug === props.currentProj);
 
-  console.log("items ", items);
-  console.log("currentProj ", currentProj);
+  // console.log("items ", items);
+  // console.log("currentProj ", currentProj);
 
   let tagsOfCurrentProj = currentProj.data.project_keys.tags;
 
-  console.log("tagsOfCurrentProj", tagsOfCurrentProj);
+  // console.log("tagsOfCurrentProj", tagsOfCurrentProj);
 
   let similarProjects = allProjects
     .filter(proj => proj.slug !== currentProj.slug)
@@ -33,10 +33,10 @@ export default function SimilarProjects(props) {
   similarProjects.sort((a, b) => b.matchingTagsCount - a.matchingTagsCount);
   similarProjects = similarProjects.slice(0, items);
 
-  console.log("Similar projects:", similarProjects);
+  // console.log("Similar projects:", similarProjects);
 
   const test = similarProjects.map(p => p.proj.data.titleimg.img.src);
-  console.log("test", test);
+  // console.log("test", test);
 
   return (
     <div className="similar_projects">
