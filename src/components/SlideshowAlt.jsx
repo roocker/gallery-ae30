52f6f3473f00ed11793a25ff50f9970d5e64249c
roocker <flosspam@gmail.com>
2024-02-12 17:30:28 +0100

    new: added img sources to projects

--- src/components/SlideshowAlt.jsx
+++ src/components/SlideshowAlt.jsx
@@ -1,8 +1,28 @@
 import { useStore } from "@nanostores/react";
-import { slideshowCurrentAlt } from "../states";
+import {
+  slideshowCurrentAlt,
+  stateSlideshow,
+  stateSlideshowIndex,
+} from "../states";
+import "../styles/source.css";
 
-function SlideshowAlt() {
+function SlideshowAlt({ sources, sources_plans }) {
   const alt = useStore(slideshowCurrentAlt);
-  return <span className="slideshow_alt">{alt}</span>;
+  const index = useStore(stateSlideshowIndex);
+  const pToggle = useStore(stateSlideshow);
+
+  return (
+    <>
+      {sources && (
+        <span className="source">
+          © {pToggle && <a href={sources[1][index]}>{sources[0][index]}</a>}
+          {!pToggle && (
+            <a href={sources_plans[1][index]}>{sources_plans[0][index]}</a>
+          )}
+        </span>
+      )}
+      <span className="slideshow_alt">{alt}</span>
+    </>
+  );
 }
 export default SlideshowAlt;
