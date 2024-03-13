7e66434b09d9343083ce09a1acca53e1d8201dec
roocker <flosspam@gmail.com>
2024-03-13 14:47:26 +0100

    edit: trying to fix live filter frontend bug

--- src/components/GridControlWrapper.jsx
+++ src/components/GridControlWrapper.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
-import GridFilter from '../components/GridFilter';
-import { AnimatePresence, motion } from 'framer-motion';
+import React, { useEffect } from "react";
+import GridFilter from "../components/GridFilter";
+import { AnimatePresence, motion } from "framer-motion";
 import { useStore } from "@nanostores/react";
-import { stateFilter, stateCurrentProjs, handleToggleFilter, stateSelectedCat } from '../states';
+import {
+  stateFilter,
+  stateCurrentProjs,
+  handleToggleFilter,
+  stateSelectedCat,
+} from "../states";
 
-import { CURRENTYEAR } from '../consts';
-import '../styles/grid_controls.css'
-import '../styles/btn.css'
+import { CURRENTYEAR } from "../consts";
+import "../styles/grid_controls.css";
+import "../styles/btn.css";
 
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
 
 const slideUp = {
   i: {
@@ -29,17 +34,16 @@ const slideUp = {
     y: "100vh",
     opacity: 0,
     transition: {
-      duration: .2,
+      duration: 0.2,
       type: "spring",
       damping: 25,
       stiffness: 400,
     },
   },
-}
+};
 
-function GridControlWrapper (props){
-
-  const allProjects = props.projects
+function GridControlWrapper(props) {
+  const allProjects = props.projects;
 
   /* console.log( "WRAPPER allProjects" , allProjects.map((proj,i) => (
     {
@@ -48,77 +52,68 @@ function GridControlWrapper (props){
     }
   ))); */
 
-  useEffect (() => {
-
-    const handleKeyDown = (event) => {
-      switch(event.key) {
-        case 'f':
+  useEffect(() => {
+    const handleKeyDown = event => {
+      switch (event.key) {
+        case "f":
           handleToggleFilter();
           break;
-        case 'Escape':
+        case "Escape":
           handleClose();
           break;
         default:
           break;
       }
-    }
+    };
 
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   });
   const tFilter = useStore(stateFilter);
-  const currentProjs = useStore(stateCurrentProjs)
+  const currentProjs = useStore(stateCurrentProjs);
 
   const handleClose = () => {
     stateFilter.set(false);
-  }
+  };
 
   // console.log("hallo", currentProjs )
 
-
-  const defaultCat = props.defaultCat
-  const defaultTag = props.defaultTag
-  const defaultYear1 = props.defaultYear1
-  const defaultYear2 = props.defaultYear2
-  const defaultSize1 = props.defaultSize1
-  const defaultSize2 = props.defaultSize2
+  const defaultCat = props.defaultCat;
+  const defaultTag = props.defaultTag;
+  const defaultYear1 = props.defaultYear1;
+  const defaultYear2 = props.defaultYear2;
+  const defaultSize1 = props.defaultSize1;
+  const defaultSize2 = props.defaultSize2;
 
   useEffect(() => {
-    if (defaultCat !== "all"){
-      stateSelectedCat.set(defaultCat)
+    if (defaultCat !== "all") {
+      stateSelectedCat.set(defaultCat);
       // stateCurrentProjs.set()
     }
-  }, [defaultCat, currentProjs])
+  }, [defaultCat, currentProjs]);
 
-  const currentProjsLength = currentProjs.length
+  const currentProjsLength = currentProjs.length;
 
-
-  return(
-
-
+  return (
     <section
-      className={`controls ${tFilter ? 'bgwhite' : ''}`}
+      className={`controls ${tFilter ? "bgwhite" : ""}`}
       aria-label="Image Grid Controls"
     >
-
       {!tFilter && (
         <div className="line select_line filter_toggle_btn">
-
           <button
             onClick={handleToggleFilter}
             className="btn filter_toggle_btn"
             title={props.btnname}
           >
             <svg className="btn_svg">
-              <use className="btn_use" href="/svg.svg#filter"/>
+              <use className="btn_use" href="/svg.svg#filter" />
             </svg>
-
           </button>
         </div>
       )}
-
 
       {tFilter && (
         <motion.div
@@ -130,7 +125,6 @@ function GridControlWrapper (props){
           animate="a"
           exit="e"
         >
-
           <GridFilter
             allProjects={allProjects}
             defaultCat={defaultCat}
@@ -140,24 +134,28 @@ function GridControlWrapper (props){
             defaultSize1={defaultSize1}
             defaultSize2={defaultSize2}
           />
-
         </motion.div>
       )}
-
 
       <div className="line counter_line">
         <p className="counter_p">
           <span className="counter_number index">
-            {`${currentProjs ? currentProjsLength.toString().padStart(2, '0') : '00'}`}
-          </span> / <span className="counter_number length">{allProjects.length > 9 ? allProjects.length : "0" + allProjects.length}</span>
+            {`${
+              currentProjs
+                ? currentProjsLength.toString().padStart(2, "0")
+                : "00"
+            }`}
+          </span>{" "}
+          /{" "}
+          <span className="counter_number length">
+            {allProjects.length > 9
+              ? allProjects.length
+              : "0" + allProjects.length}
+          </span>
         </p>
       </div>
-
-
-
     </section>
+  );
+}
 
-  )
-};
-
-export default GridControlWrapper
+export default GridControlWrapper;
