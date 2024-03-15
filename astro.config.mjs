12344e600c03c435feed266d4d28a6f1d27b3cbf
roocker <flosspam@gmail.com>
2024-03-15 14:14:59 +0100

    edit: serve @ ae30.roocker.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,13 @@ import react from "@astrojs/react";
 // https://decapcms.org/docs/beta-features/#folder-collections-media-and-public-folder
 // import netlify from "@astrojs/netlify";
 // import node from "@astrojs/node";
-
+// import { loadEnv } from "vite";
+// const { BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ae30.at",
-  // base: process.env.BASE_URL || "/",
-  base: "/ae30",
+  site: "https://p.roocker.dev/ae30/",
+  // base: BASE_URL,
+  // base: "/ae30",
   // output: "hybrid",
   // adapter: netlify({ edgeMiddleware: true }),
   /* adapter: node({
